824d4478b3a9f1dceba35ac11dd7a573c82dc110
senoajisr <xsenoajix@gmail.com>
2025-11-17 15:02:58 +0700

    add: logging

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import rle_iterative
 import rle_recursive
+import logging
 
 def main() -> None:
-    pass
+    initialize_logging()
+
+
+def initialize_logging() -> None:
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.info("Logging initialized.")
+
 
 if __name__ == "__main__":
     main()
