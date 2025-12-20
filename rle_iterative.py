185e3798105801b04911f92edc4cb807b4d4633a
senoajisr <xsenoajix@gmail.com>
2025-12-20 17:10:01 +0700

    fix: IndexError for RLE that have 10+ compression

--- rle_iterative.py
+++ rle_iterative.py
@@ -19,8 +19,17 @@ def encode(text: str) -> str:
 
 def decode(text: str) -> str:
     result: str = ''
+    character: str = text[0]
+    count: str = ''
     
-    for i in range(0, len(text), 2):
-        result += text[i] * int(text[i+1])
+    for i in range(1, len(text)):
+        if not text[i].isdigit():
+            result += character * int(count)
+            character=text[i]
+            continue
+        
+        count += text[i]
+    
+    result += character * int(count)
     
     return result
