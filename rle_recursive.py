67adac8fbaeb78e8b2fa67bd5149e7608a68d314
senoajisr <xsenoajix@gmail.com>
2025-12-29 14:21:31 +0700

    remove: unnecesary i += 1 line

--- rle_recursive.py
+++ rle_recursive.py
@@ -4,18 +4,17 @@ def guarded_encode(text: str, count: int = 1, i: int = 0) -> str|None:
     
     return encode(text, count, i)
 
-def encode(text: str, count: int = 1, i: int = 0) -> str:
+def encode(text: str, count: int = 1, i: int = 1) -> str:
     text_length = len(text)
-    i += 1
     
     if text_length == i:
         return text[-1] + str(count)
     
     if text[i] == text[i - 1]: 
         count+=1
-        return encode(text, count, i)
+        return encode(text, count, i+1)
     
-    return text[i - 1] + str(count) + encode(text, 1, i)
+    return text[i - 1] + str(count) + encode(text, 1, i+1)
 
 
 def decode(text: str, character: str = "", count: str = "", i: int = 1) -> str:
